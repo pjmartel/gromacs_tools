d44d6e24b84bdb9303b7b6b5a142177a0ca71164
Paulo Martel <paulo.martel@gmail.com>
2025-11-30 21:51:17 +0000

    Refactor initial segment execution in continue_md_grompp_simple.sh for clarity and consistency

--- continue_md_grompp_simple.sh
+++ continue_md_grompp_simple.sh
@@ -16,13 +16,16 @@ steps=$((dt*500000))  # assumes a 2fs timestep
 
 # Initial segment.  Run once, for tstart=0
 if [ $tstart -eq 0 ]; then
+    echo -e "\n****** Running initial segment: 0 ns to ${dt} ns ******\n"
+    # Create mdp file for initial segment from template
     sed  -e "s/\(steps\s\+= \)[0-9]\+/\1${steps}/" ${base_name}_0_0.mdp > ${base_name}_0_${dt}.mdp
     gmx grompp -f ${base_name}_0_${dt}.mdp -c ${base_name}_0_0.gro -t ${base_name}_0_0.cpt -e ${base_name}_0_0.edr -p topol.top -o ${base_name}_0_${dt}.tpr
-    gmx mdrun -deffnm ${base_name}_0_${dt} 
+    gmx mdrun -deffnm ${base_name}_0_${dt}
+    # Increment start time for following segments 
     tstart=$((tstart+dt))
 fi
 
-# following segments run on loop
+# Continuation segments
 for ((cur_time=$tstart ; cur_time<$tend ; cur_time+=$dt)) ; do
 
     echo -e "\n****** Running segment: ${cur_time} ns to $((cur_time+dt)) ns ******\n"
@@ -37,10 +40,13 @@ for ((cur_time=$tstart ; cur_time<$tend ; cur_time+=$dt)) ; do
 
     timeps=$((1000*cur_time)) # time in mdp is in picoseconds ! 
 
+    # Fix tinit for mdp file of current segment
     sed  -e "s/\(tinit\s\+= \)[0-9]\+/\1${timeps}/" ${prev}.mdp > ${cur}.mdp
 
     gmx grompp -f ${cur}.mdp -c ${prev}.gro -t ${prev}.cpt -e ${prev}.edr -p topol.top -o ${cur}.tpr
     gmx mdrun -deffnm $cur
+
+    # Check for STOP file to halt further processing
     if [[ -f ./STOP ]] ; then
         echo "Found STOP file. Stopping."
         break
